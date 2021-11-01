9937d80c5818e94615127018be1a692fbac4f71a
mj2360 <mj2360@nyu.edu>
2021-11-01 17:57:06 -0400

    final touches on styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,34 +1,24 @@
-import React from 'react'; 
+import React from "react";
 
-function Display ({
-    aWord, 
-    pos, 
-    definition, 
-    gifUrl,
-}) {
-    return(
-        <section className="displayBox">
-            <div className="Wrapper">
-                <div className="wordWrapper">
-                <h1 className="word">
-                    {aWord}
-                </h1>
+function Display({ aWord, pos, definition, gifUrl }) {
+  return (
+    <section className="displayBox">
+      <div className="Wrapper">
+        <div className="wordWrapper">
+          <h1 className="word">{aWord}</h1>
 
-                <p className="POS">
-                    {pos}
-                </p>
-                </div>
-
-                <p className="definition">
-                 {definition}
-                </p> 
-                
-                <div className="imageWrapper">
-                    <img src={gifUrl} alt={aWord} className="gif"/>
-                </div>
-            </div>
-        </section>
-    )
+          <p className="POS">{pos}</p>
+        </div>
+        <div className="resWrapper">
+          <h2 className="defOf">Defintion of {aWord}</h2>
+          <p className="definition">: {definition}</p>
+          <div className="imageWrapper">
+            <img src={gifUrl} alt={aWord} className="gif" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Display; 
+export default Display;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,73 +1,79 @@
-import React, { useState, useMemo} from 'react'; 
-import axios from 'axios'; 
-import Display from '../components/Display'; 
+import React, { useState, useMemo } from "react";
+import axios from "axios";
+import Display from "../components/Display";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
-  const[wordData, setWordData] = useState(); 
-  const[gif, setGifData] = useState(); 
+  const [wordData, setWordData] = useState();
+  const [gif, setGifData] = useState();
 
   function changeWord(e) {
-    e.preventDefault(); 
+    e.preventDefault();
 
-    const wordInput = e.currentTarget.word.value 
+    const wordInput = e.currentTarget.word.value;
     console.log(wordInput);
 
-    const url_1 = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput}`
-    const url_2 = `http://api.giphy.com/v1/gifs/translate?s=${wordInput}&api_key=k364FjEl3SMVa01bo63nZWqlaTDQdQTN&weirdness=10`
-    axios.get(url_1)
-    .then(function (response) {
-      setWordData(response.data);
-      return axios.get(url_2)
+    const url_1 = `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput}`;
+    const url_2 = `http://api.giphy.com/v1/gifs/translate?s=${wordInput}&api_key=k364FjEl3SMVa01bo63nZWqlaTDQdQTN&weirdness=10`;
+    axios
+      .get(url_1)
       .then(function (response) {
-        setGifData(response.data);
+        setWordData(response.data);
+        return axios.get(url_2).then(function (response) {
+          setGifData(response.data);
+        });
       })
-    })
-    .catch(function (error) {
-      console.warn(error);
-    });
-    }
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }
 
-    const { 
-      aWord, 
-      pos, 
-      definition,
-     } = useMemo(() => {
-      if (!wordData) return{};
-  
-      return{
-        aWord: wordData[0].word, 
-        pos: wordData[0].meanings[0].partOfSpeech,
-        definition: wordData[0].meanings[0].definitions[0].definition, 
-      }
-    }, [wordData])
+  const { aWord, pos, definition } = useMemo(() => {
+    if (!wordData) return {};
 
-    const gifItem = useMemo(() => {
-      if (!gif) return{};
-  
-      return{
-        gifItem: gif.data.images.fixed_height.url,
-       }
-      }, [gif])
+    return {
+      aWord: wordData[0].word,
+      pos: wordData[0].meanings[0].partOfSpeech,
+      definition: wordData[0].meanings[0].definitions[0].definition,
+    };
+  }, [wordData]);
 
-    const gifUrl = gifItem.gifItem; 
+  const gifItem = useMemo(() => {
+    if (!gif) return {};
 
-    return (
-      <>
+    return {
+      gifItem: gif.data.images.fixed_height.url,
+    };
+  }, [gif]);
+
+  const gifUrl = gifItem.gifItem;
+
+  return (
+    <>
+      <div className="wrapper">
         <form onSubmit={(e) => changeWord(e)}>
-        <input type="text" placeholder="Type Word" id="input" name="word" />
-        <button type="submit">Submit</button>
+          <input
+            className="search"
+            type="text"
+            placeholder="Type Word"
+            id="input"
+            name="word"
+          />
+          <button className="submit" type="submit">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
         </form>
+      </div>
 
-        <Display 
-          aWord={aWord}
-          pos={pos}
-          definition={definition}
-          gifUrl={gifUrl}
-        /> 
-
-      </>
-    );
-   
+      <Display
+        aWord={aWord}
+        pos={pos}
+        definition={definition}
+        gifUrl={gifUrl}
+      />
+    </>
+  );
 }
 
-export default Home; 
+export default Home;
